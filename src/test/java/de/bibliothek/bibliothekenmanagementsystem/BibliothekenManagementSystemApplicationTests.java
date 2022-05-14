56fcbefc4c68046049f6d72d4db18d480252b1e0
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-05-14 21:38:50 +0200

    initial Projekt

---
+++ src/test/java/de/bibliothek/bibliothekenmanagementsystem/BibliothekenManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package de.bibliothek.bibliothekenmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliothekenManagementSystemApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
